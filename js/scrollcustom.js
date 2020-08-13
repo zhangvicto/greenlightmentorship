bd35cf4dbb00b2fd010713222cdf1ea7d194d616
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2020-08-13 00:36:45 -0400

    Update

--- js/scrollcustom.js
+++ js/scrollcustom.js
@@ -1,5 +1,5 @@
-let scrollItem = document.getElementsByClassName("page");
-console.log(scrollItem);
-for (let x = 0;x<scrollItem.length;x++) {
-    scrollItem[x].element.scrollIntoView({behavior: "smooth"});
-}
+// let scrollItem = document.getElementsByClassName("page");
+// console.log(scrollItem);
+// for (let x = 0;x<scrollItem.length;x++) {
+//     scrollItem[x].element.scrollIntoView({behavior: "smooth"});
+// }
